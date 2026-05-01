98d989f60ce107ec6db4a748e4d9fd707e05c4db
Gravios <justingraboski@gmail.com>
2026-05-01 17:32:40 +0200

    round 8 session lists

--- neurobox/config/__init__.py
+++ neurobox/config/__init__.py
@@ -11,6 +11,16 @@ from .config import (
     link_session_status,
     load_config,
 )
+from .session_lists import (
+    SessionList,
+    TrialSpec,
+    SubjectInfo,
+    HeadCorrection,
+    ChannelGroup,
+    AnatLocation,
+    available_session_lists,
+    load_session_list,
+)
 __all__ = [
     "configure_project",
     "discover_mazes",
@@ -18,4 +28,12 @@ __all__ = [
     "link_sessions",
     "link_session_status",
     "load_config",
+    "SessionList",
+    "TrialSpec",
+    "SubjectInfo",
+    "HeadCorrection",
+    "ChannelGroup",
+    "AnatLocation",
+    "available_session_lists",
+    "load_session_list",
 ]
